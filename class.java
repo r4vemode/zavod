c884925f1f77179a5eaad64564dea06b6ba41971
r4vemode <62845988+r4vemode@users.noreply.github.com>
2022-01-13 03:07:49 +0300

    Update class.java

--- class.java
+++ class.java
@@ -5,58 +5,65 @@ public abstract class Zavod{
 }
 public abstract class Workers {
 
-   private String name;
-   private int age;
-   private String nationality;
-   private String efficiency;
    
-       public abstract void Michail(){
-     name = "Michail";
-     age = 26;
-     nationality = "russian";
-     efficiency = 5,5;
-   }
+   Workers(String name, int age, string nationality, string efficiency) {
+this.name = name
+this.age = age
+this.nationality = nationality
+this.efficiency = efficiency
+}
+   //private String name;
+//    private int age;
+//    private String nationality;
+//    private String efficiency;
+   
+//        public abstract void Michail(){
+//      name = "Michail";
+//      age = 26;
+//      nationality = "russian";
+//      efficiency = 5,5;
+//    }
 
-       public abstract void Ivan(){
-     name = "Ivan";
-     age = 22;
-     nationality = "russian";
-     efficiency = 1,5;
-   }
-       public abstract void Semen(){
-     name = "Semen";
-     age = 46;
-     nationality = "russian";
-     efficiency = 8,5;
-   }    
+//        public abstract void Ivan(){
+//      name = "Ivan";
+//      age = 22;
+//      nationality = "russian";
+//      efficiency = 1,5;
+//    }
+//        public abstract void Semen(){
+//      name = "Semen";
+//      age = 46;
+//      nationality = "russian";
+//      efficiency = 8,5;
+//    }    
        
-       public abstract void Alesha(){
-     name = "Alesha";
-     age = 39;
-     nationality = "russian";
-     efficiency = 7;
-   }     
+//        public abstract void Alesha(){
+//      name = "Alesha";
+//      age = 39;
+//      nationality = "russian";
+//      efficiency = 7;
+//    }     
          
-       public abstract void Sofia(){
-     name = "Sofia";
-     age = 33;
-     nationality = "argentine";
-     efficiency = 6,5;
-   }      
+//        public abstract void Sofia(){
+//      name = "Sofia";
+//      age = 33;
+//      nationality = "argentine";
+//      efficiency = 6,5;
+//    }      
            
-       public abstract void Carlos (){
-     name = "Carlos";
-     age = 50;
-     nationality = "russian";
-     efficiency = 9;
-   }          
+//        public abstract void Carlos (){
+//      name = "Carlos";
+//      age = 50;
+//      nationality = "russian";
+//      efficiency = 9;
+//    }          
              
-       public abstract void Mark(){
-     name = "Mark";
-     age = 54;
-     nationality = "russian";
-     efficiency = 9,5;
-   }
+//        public abstract void Mark(){
+//      name = "Mark";
+//      age = 54;
+//      nationality = "russian";
+//      efficiency = 9,5;
+  // }
    
 }
 public abstract class ent{
