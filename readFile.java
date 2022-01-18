777a069b8b58e82824412639402fad00638dd507
r4vemode <62845988+r4vemode@users.noreply.github.com>
2022-01-18 13:58:18 +0300

    Update readFile.java

--- readFile.java
+++ readFile.java
@@ -1,17 +1,22 @@
-public class ReadFromFile {
-  public static void main(String[] args) throws FileNotFoudException{
-    String separator = File.separator;
-    String path = separator + "" + separator + "" +  separator + "";
-    
-    File file = new Scanenr(file);
-    
-    Scanner scanner = new Scanner(File);
-    String line = scanner.nextLine();
-    String[] words = line.split("");
-    
-    System.out.println(Arrays.toString(words));
-    
-    scanner.close();
-  }
-  
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Factory[] fact;
+        FileReader reader = new FileReader("/Users/egorvolosenko/IdeaProjects/000/src/cards.json");
+        JsonReader jsonreader = new JsonReader(reader);
+        Gson gson = new Gson();
+        fact = gson.fromJson(jsonreader,Factory[].class);
+        for(Factory fac:fact){
+            System.out.println(fac.cards);
+        }
+
+    }
+  public class Factory {
+    String Cards;
+}
 }
